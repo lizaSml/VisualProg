9ea5fa85a1e2c31f306889bd983cb94f5794e341
Liza <lizasm2018@mail.ru>
2024-10-24 00:28:58 +0700

    Adding a file mainwindow.cpp with design settings and attached header files

---
+++ mainwindow.cpp
@@ -0,0 +1,85 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QInputDialog>
+#include <QFontDialog>
+#include <QColorDialog>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QTextDocument>
+#include <QSettings>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QCloseEvent>
+#include <QFileInfo>
+#include "TableInputDialog.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , isTempFileEmpty(true)
+    , isContentModified(false)
+    , tempFile(QDir::tempPath() + "/tempfile.html")
+{
+    ui->setupUi(this);
+    loadSettings();
+
+    textEdit = new QTextEdit(this);
+    setCentralWidget(textEdit);
+
+    connect(ui->Open, &QAction::triggered, this, &MainWindow::on_openFile_triggered);
+    connect(ui->Save, &QAction::triggered, this, &MainWindow::on_saveFile_triggered);
+    connect(ui->Clear, &QAction::triggered, this, &MainWindow::clearAndSave);
+    connect(ui->Return, &QAction::triggered, this, &MainWindow::restoreFromTemp);
+    connect(ui->actionChangeFont, &QAction::triggered, this, &MainWindow::on_fontChange_triggered);
+    connect(ui->actionChangeColor, &QAction::triggered, this, &MainWindow::on_colorChange_triggered);
+    connect(ui->Search, &QAction::triggered, this, &MainWindow::on_search_triggered);
+    connect(ui->Replace, &QAction::triggered, this, &MainWindow::on_replace_triggered);
+    connect(ui->Create, &QAction::triggered, this, &MainWindow::on_createTable_triggered);
+
+    ui->actionChangeFont->setShortcut(QKeySequence("Ctrl+I"));
+    ui->actionChangeColor->setShortcut(QKeySequence("Ctrl+O"));
+    ui->Open->setShortcut(QKeySequence("Ctrl+R"));
+    ui->Save->setShortcut(QKeySequence("Ctrl+S"));
+    ui->Clear->setShortcut(QKeySequence("Ctrl+U"));
+    ui->Return->setShortcut(QKeySequence("Ctrl+P"));
+    ui->Search->setShortcut(QKeySequence("Ctrl+E"));
+    ui->Replace->setShortcut(QKeySequence("Ctrl+W"));
+
+    QLabel *backgroundLabel = new QLabel(this);
+    backgroundLabel->setScaledContents(true);
+    backgroundLabel->setGeometry(this->rect());
+    backgroundLabel->lower();
+
+    ui->Return->setEnabled(false);
+
+    QApplication* app = qApp;
+    app->setStyleSheet(
+        "QLabel {"
+        "   font-size: 14px;"
+        "   color: #ff0f23;"
+        "}"
+        "QLineEdit {"
+        "   font-size: 14px;"
+        "   padding: 5px;"
+        "   border: 1px solid #ffc0cb;"
+        "   border-radius: 4px;"
+        "}"
+        "QPushButton {"
+        "   font-size: 14px;"
+        "   padding: 8px 16px;"
+        "   background-color: #ffc0cb;"
+        "   color: white;"
+        "   border: none;"
+        "   border-radius: 4px;"
+        "}"
+        "QPushButton:hover {"
+        "   background-color: #ffa8af;"
+        "}"
+        );
+}
